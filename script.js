4bd679069f761a91235c388e02d3ca424ab5aab5
dustinmclean <148822306+dustinmclean@users.noreply.github.com>
2024-03-15 10:33:59 -0700

    Create index.html, sscript.js, style.css, create initial layout

---
+++ script.js
@@ -0,0 +1,9 @@
+function eatShit(butt) {
+    if (butt.includes("poop")) {
+        console.log("something stinks")
+    } else {
+        console.log("no poop here")
+    }
+}
+
+eatShit("someone farted")
